1bfab568692974c4d89c8961749385d727d978ce
nxfive <kontakt@nxfive.pl>
2024-08-19 22:55:16 +0200

    feat: Create LeadStatus and add status to Lead model

--- leads/models.py
+++ leads/models.py
@@ -13,11 +13,18 @@ class Lead(models.Model):
 
         def __str__(self):
             return f"{self.get_product_display()}"
+        
+    class LeadStatus(models.TextChoices):
+        NEW = "New", _("New")
+        CONTACTED = "Contacted", _("Contacted")
+        FOLLOW_UP = "Follow-up", _("Follow_up")
+        CLOSED = "Closed", _("Closed")
 
     first_name = models.CharField(max_length=25)
     phone_number = models.CharField(max_length=25)
     product = models.CharField(max_length=25, choices=FinancialProducts.choices)
     message = models.TextField(max_length=500, null=True, blank=True)
+    status = models.CharField(max_length=15, choices=LeadStatus.choices, default=LeadStatus.NEW)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     description = models.TextField(max_length=500)
