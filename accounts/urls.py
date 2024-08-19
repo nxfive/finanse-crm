20d4a5474dd9266996cd76162f2708839f9865ae
nxfive <kontakt@nxfive.pl>
2024-08-19 21:32:21 +0200

    feat: Create path for list_accounts view

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path("", views.list_accounts, name="account-list"),
     path("dashboard/<slug:username>/", views.dashboard, name="dashboard"),
     path("login/", views.custom_login, name="login"),
 ]
