77043bd9ec90fe6c6bdfac4ae9ee3da61ae6621f
nxfive <kontakt@nxfive.pl>
2024-08-19 19:46:24 +0200

    feat: Create Account model with Account Manager

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,66 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class AccountManager(BaseUserManager):
+    def create_user(self, first_name, last_name, username, email, password=None):
+        if not email:
+            raise ValueError("User must have an email address")
+        
+        if not username:
+            raise ValueError("User must have a username")
+        
+        user = self.model(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, first_name, last_name, username, email, password):
+        user = self.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=self.normalize_email(email),
+            password=password,
+        )
+        
+        user.is_active = True
+        user.is_staff = True 
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+    
+
+class Account(AbstractBaseUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=50, null=True, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ("username", "fist_name", "last_name",) 
+
+    objects = AccountManager()
+
+    def __str__(self):
+        return f"admin: {self.first_name} {self.last_name}"
+    
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
