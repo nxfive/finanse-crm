70e149cce26e108538f45238a2930a707791cbb6
nxfive <kontakt@nxfive.pl>
2024-08-19 17:43:23 +0200

    feat: Add settings: allow * all hosts, add templates dir

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-%%%l!%_7ya#wdr27&_kp2awj#b1sdehqc2z=9)9@=lh_ttm^%!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = "djcrm.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
