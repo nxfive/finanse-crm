d172e1e0285bd4ef7da65a23eb6b205b96f2da36
nxfive <kontakt@nxfive.pl>
2024-08-19 16:55:12 +0200

    feat: Create Lead model

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=25)
+    phone_number = models.CharField(max_length=25)
+    message = models.TextField(max_length=500, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    description = models.TextField(max_length=500)
+    email = models.EmailField(unique=True, null=True, blank=True)
+
+    class Meta:
+        ordering = ("-created_at",)
+
+    def __str__(self):
+        return f"{self.first_name}: {self.phone_number}"
