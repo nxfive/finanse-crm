ec4e5c401a8dd5ca6dafb778d21a079e41ac4e23
nxfive <kontakt@nxfive.pl>
2024-08-19 22:22:27 +0200

    feat: Create form for update Lead model

--- leads/forms.py
+++ leads/forms.py
@@ -6,3 +6,14 @@ class LeadCreateForm(forms.ModelForm):
     class Meta:
         model = Lead
         fields = ("first_name", "phone_number", "message", )
+
+
+class LeadUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Lead
+        fields = ("fist_name", "description", "email", )
+        widgets = {
+            "first_name": forms.TextInput(attrs={"placeholder": "Enter lead name"}),
+            "description": forms.Textarea(attrs={"placeholder": "Enter more information about the lead"}),
+            "email": forms.EmailInput(attrs={"placeholder": "Enter lead email"})
+        }
