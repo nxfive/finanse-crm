3377162e3979ada9aab0bfeb92e465ab6bcc1bb9
nxfive <kontakt@nxfive.pl>
2024-08-19 22:33:58 +0200

    feat: Create update,delete,detail,list,create view for Lead model

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from .forms import LeadCreateForm, LeadUpdateForm
+from django.views.generic import (
+    CreateView,
+    UpdateView,
+    DeleteView,
+    ListView,
+    DetailView,
+)
 
-# Create your views here.
+from .models import Lead
+
+
+class LeadListView(ListView):
+    model = Lead
+    template_name = "leads/lead_list.html"
+    context_object_name = "leads"
+
+
+class LeadCreateView(CreateView):
+    form_class = LeadCreateForm
+    template_name = "leads/lead_create.html"
+    success_url = reverse_lazy("leads:lead-list")
+
+
+class LeadUpdateView(UpdateView):
+    form_class = LeadUpdateForm
+    template_name = "leads/lead_update.html"
+
+    def get_success_url(self) -> str:
+        return reverse_lazy("leads:lead-detail", kwargs={"pk": self.kwargs["pk"]})
+
+
+class LeadDetailView(DetailView):
+    model = Lead
+    template_name = "leads/lead_detail.html"
+    context_object_name = "lead"
+
+
+class LeadDeleteView(DeleteView):
+    model = Lead
+    template_name = "leads/lead_delete.html"
+    context_object_name = "leads"
+    success_url = reverse_lazy("leads:lead-list")
