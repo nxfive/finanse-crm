8cf42d4a73310e8a3c8d3f50dae09773c77a24d8
nxfive <kontakt@nxfive.pl>
2024-08-20 20:20:50 +0200

    feat: Create Manager model

--- managers/models.py
+++ managers/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your models here.
+from accounts.models import Account
+
+class Manager(models.Model):
+    user = models.OneToOneField(Account, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+    
+    def get_team(self):
+        try:
+            return self.team
+        except ObjectDoesNotExist:
+            return None
