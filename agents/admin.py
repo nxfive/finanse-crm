7984c091d3019e4582d2d73f1542ff7dade3eb7c
nxfive <kontakt@nxfive.pl>
2024-08-20 16:21:08 +0200

    feat: Customize AgentAdmin with user attrs and create/update form

--- agents/admin.py
+++ agents/admin.py
@@ -1,3 +1,34 @@
+from typing import Any
 from django.contrib import admin
+from .forms import AgentCreateForm, AgentUpdateForm
+from .models import Agent
 
-# Register your models here.
+
+class AgentAdmin(admin.ModelAdmin):
+    form = AgentUpdateForm
+    add_form = AgentCreateForm
+
+    list_display = ("get_user_last_name", "get_user_first_name", "get_user_email", "birth_date", "role",)
+
+
+    def get_user_last_name(self, obj: Agent) -> str:
+        return obj.user.last_name
+    
+    def get_user_first_name(self, obj: Agent) -> str:
+        return obj.user.first_name
+    
+    def get_user_email(self, obj: Agent) -> str:
+        return obj.user.email
+    
+    get_user_last_name.short_description = "Last Name"
+    get_user_first_name.short_description = "First Name"
+    get_user_email.short_description = "Email"
+
+
+    def get_form(self, request: Any, obj: Any | None = ..., change: bool = ..., **kwargs: Any) -> Any:
+        if obj is None:
+            return self.add_form
+        return self.form
+
+
+admin.site.register(Agent, AgentAdmin)
