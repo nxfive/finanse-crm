1fe52c68fd5f1e738dadc49dcb74bc20e948cad1
nxfive <kontakt@nxfive.pl>
2024-08-20 14:28:47 +0200

    feat: Customize LeadAdmin with separate forms for creation and update

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,32 @@
+from typing import Any
 from django.contrib import admin
+from .models import Lead
+from .forms import LeadUpdateForm, LeadCreateForm
 
-# Register your models here.
+
+class LeadAdmin(admin.ModelAdmin):
+    form = LeadUpdateForm
+    add_form = LeadCreateForm
+    list_display = (
+        "first_name",
+        "phone_number",
+        "product",
+        "status",
+        "created_at",
+        "updated_at",
+    )
+    list_display_links = (
+        "first_name",
+        "phone_number",
+    )
+    search_fields = ("phone_number",)
+
+    def get_form(
+        self, request: Any, obj: Any | None = ..., change: bool = ..., **kwargs: Any
+    ) -> Any:
+        if obj is None:
+            return self.add_form
+        return self.form
+
+
+admin.site.register(Lead, LeadAdmin)
