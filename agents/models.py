82ca02e5a9bd1552e8d9a28df04b613df0cac8b8
nxfive <kontakt@nxfive.pl>
2024-08-20 14:57:08 +0200

    feat: Create model Agent with Roles choices for attr role

--- agents/models.py
+++ agents/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from accounts.models import Account
 
-# Create your models here.
+
+class Agent(models.Model):
+    class Roles(models.TextChoices):
+        NONE = "", _("Select Role")
+        SALES = "Sales Agent", _("Sales Agent")
+        SUPPORT = "Sales Support Agent", _("Sales Suport Agent")
+    
+    user = models.OneToOneField(Account, on_delete=models.CASCADE)
+    birth_date = models.DateField()
+    role = models.CharField(max_length=30, choices=Roles.choices)
+
+    class Meta:
+        ordering = ("user__last_name", "user__first_name", )
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
