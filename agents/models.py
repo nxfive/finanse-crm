91941590c79e0664eeeb788cad15f73b8e724d70
nxfive <kontakt@nxfive.pl>
2024-08-20 19:57:31 +0200

    feat: Add team field to Agent model with ForeignKey to Team

--- agents/models.py
+++ agents/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from accounts.models import Account
+from teams.models import Team
 
 
 class Agent(models.Model):
@@ -12,6 +13,7 @@ class Agent(models.Model):
     user = models.OneToOneField(Account, on_delete=models.CASCADE)
     birth_date = models.DateField()
     role = models.CharField(max_length=30, choices=Roles.choices)
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True, related_name="agents")
 
     class Meta:
         ordering = ("user__last_name", "user__first_name", )
