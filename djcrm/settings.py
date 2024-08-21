b50190e4db9a09bb0da08afcd498f0b94d06a4a7
nxfive <kontakt@nxfive.pl>
2024-08-21 12:31:11 +0200

    feat: Add custom context processors to the project

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -67,6 +67,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "teams.context_processors.team",
+                "teams.context_processors.team_manager",
             ],
         },
     },
