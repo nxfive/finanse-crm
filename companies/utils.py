4a981bddb0442191657a34d46f47e7faaa17cd6e
nxfive <kontakt@nxfive.pl>
2024-08-21 14:30:02 +0200

    feat: Create func for saving lead form from websites

---
+++ companies/utils.py
@@ -0,0 +1,26 @@
+from django.contrib import messages
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
+
+from leads.forms import CompanyLeadCreateForm
+from leads.models import LeadSubmission
+
+
+def company_lead_create(request: HttpRequest, template_name: str, succes_url: str) -> HttpResponse:
+    if request.method == "POST":
+        form = CompanyLeadCreateForm(request.POST)
+        if form.is_valid():
+            lead = form.save(commit=False)
+            lead.save(path=request.path)
+
+            LeadSubmission.objects.create(
+                lead=lead,
+                ip_address=request.META.get("REMOTE_ADDR"),
+                http_user_agent=request.META.get("HTTP_USER_AGENT"),
+            )
+
+            messages.success(request, "Thank you for your message. We will contact you shortly.")
+            return redirect(succes_url)
+    else:
+        form = CompanyLeadCreateForm()
+    return render(request, template_name, {"form": form})
