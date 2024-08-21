ad75c022555393e0bc70cc777bcf31532aff1ead
nxfive <kontakt@nxfive.pl>
2024-08-21 12:33:02 +0200

    feat: Create lead update form for agents, add docstrings

--- leads/forms.py
+++ leads/forms.py
@@ -3,6 +3,9 @@ from .models import Lead
 
 
 class LeadCreateForm(forms.ModelForm):
+    """
+        This form is intended for use by superuser and manager users only.
+    """
     class Meta:
         model = Lead
         fields = ("first_name", "phone_number", "message", "product", )
@@ -12,13 +15,21 @@ class LeadCreateForm(forms.ModelForm):
             "message": forms.Textarea(attrs={"placeholder": "Enter lead message"})
         }
 
-
-class LeadUpdateForm(forms.ModelForm):
+class LeadUpdateAgentForm(forms.ModelForm):
     class Meta:
         model = Lead
-        fields = ("first_name", "description", "email", "product", "status", )
+        fields = ("first_name", "description", "email", "product", "status",)
         widgets = {
             "first_name": forms.TextInput(attrs={"placeholder": "Enter lead name"}),
             "description": forms.Textarea(attrs={"placeholder": "Enter more information about the lead"}),
             "email": forms.EmailInput(attrs={"placeholder": "Enter lead email"})
         }
+
+
+class LeadUpdateForm(LeadUpdateAgentForm):
+    """
+        This form is intended for use by superuser and manager users only.
+    """
+
+    class Meta(LeadUpdateAgentForm.Meta):
+        fields = LeadUpdateAgentForm.Meta.fields + ("agent",)
