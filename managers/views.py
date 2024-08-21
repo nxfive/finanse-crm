9dfe830857bbee7ca57eae9eadc8a6101faa3b35
nxfive <kontakt@nxfive.pl>
2024-08-21 20:25:58 +0200

    feat: Create list/delete/detail/create views for Manager model

--- managers/views.py
+++ managers/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DeleteView, CreateView, DetailView
 
-# Create your views here.
+from .models import Manager
+from .forms import ManagerForm
+
+
+class ManagerListView(ListView):
+    model = Manager
+    template_name = "managers/manager_list.html"
+    context_object_name = "managers"
+
+
+class ManagerDeleteView(DeleteView):
+    model = Manager
+    template_name = "managers/manager_delete.html"
+
+
+class ManagerCreateView(CreateView):
+    form_class = ManagerForm
+    template_name = "managers/manager_create.html"
+    success_url = reverse_lazy("managers:manager-list")
+
+class ManagerDetailView(DetailView):
+    model = Manager
+    template_name = "managers/manager_detail.html"
