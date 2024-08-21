0be34176a3a15daea03b880427b71458fe841b1d
nxfive <kontakt@nxfive.pl>
2024-08-21 22:38:26 +0200

    feat: Create BaseUserForm to add users without roles

---
+++ core/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from accounts.models import Account
+from agents.models import Agent
+from managers.models import Manager
+
+
+class BaseUserForm(forms.ModelForm):
+    class Meta:
+        fields = ("user",)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        admins_id = list(Account.objects.filter(is_superuser=True).values_list("id", flat=True))
+        agents_id = list(Agent.objects.values_list("user__id", flat=True))
+        managers_id = list(Manager.objects.values_list("user__id", flat=True))
+
+        self.fields["user"].queryset = Account.objects.exclude(pk__in=admins_id + agents_id + managers_id)
+        self.fields["user"].empty_label = "Select User"
