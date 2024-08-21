207e01db0ffe1ad2eb1111d85a2a645ae8463e37
nxfive <kontakt@nxfive.pl>
2024-08-21 22:39:12 +0200

    feat: Inherit BaseUserForm from core app in create form

--- agents/forms.py
+++ agents/forms.py
@@ -1,22 +1,15 @@
 from django import forms
-from .models import Agent
 
-from accounts.models import Account
+from core.forms import BaseUserForm, Agent
 
-class AgentCreateForm(forms.ModelForm):
+
+class AgentCreateForm(BaseUserForm):
     class Meta:
         model = Agent
-        fields = ("user", "birth_date", "role",)
+        fields = BaseUserForm.Meta.fields + ("birth_date", "role",)
         widgets = {
             "birth_date": forms.DateInput(attrs={"type":"date"})
         }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        admins_id = list(Account.objects.filter(is_superuser=True).values_list("id", flat=True))
-        agents_id = list(Agent.objects.values_list("user__id", flat=True))
-
-        self.fields["user"].queryset = Account.objects.exclude(pk__in=admins_id + agents_id)
 
 
 class AgentUpdateForm(forms.ModelForm):
