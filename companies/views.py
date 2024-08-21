56bed7158e25abc15ba4101fe230d0e8cdcccd16
nxfive <kontakt@nxfive.pl>
2024-08-21 15:38:55 +0200

    feat: Create views for company websites

--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .utils import company_lead_create
+
+
+def bank_finanse_create(request: HttpRequest) -> HttpResponse:
+    return company_lead_create(request=request, template_name="websites/bank_finanse.html", succes_url="websites:bank-finanse-sent")
+
+def bank_finanse_sent(request: HttpRequest) -> HttpResponse:
+    return render(request=request, template_name="websites/bank_finanse_sent.html")
+
+def house_finder_create(request: HttpRequest) -> HttpResponse:
+    return company_lead_create(request=request, template_name="websites/house_finder.html", succes_url="websites:house-finder-sent")
+
+def house_finder_sent(request: HttpRequest) -> HttpResponse:
+    return render(request=request, template_name="websites/house_finder_sent.html")
