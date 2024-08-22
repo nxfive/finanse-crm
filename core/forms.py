1dfd82a94e5df19c675fc6cf2145152922c8de31
nxfive <kontakt@nxfive.pl>
2024-08-22 14:00:59 +0200

    feat: Raise exception if `BaseUserForm` is instantiated directly

--- core/forms.py
+++ core/forms.py
@@ -9,7 +9,10 @@ class BaseUserForm(forms.ModelForm):
         fields = ("user",)
 
     def __init__(self, *args, **kwargs):
+        if self.__class__ == BaseUserForm:
+            raise NotImplementedError("BaseUserForm cannot be instantiated directly.")
         super().__init__(*args, **kwargs)
+        
         admins_id = list(Account.objects.filter(is_superuser=True).values_list("id", flat=True))
         agents_id = list(Agent.objects.values_list("user__id", flat=True))
         managers_id = list(Manager.objects.values_list("user__id", flat=True))
