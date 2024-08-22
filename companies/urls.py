b3b03958ef168a4b9050c556f74daed3a17d2b90
nxfive <kontakt@nxfive.pl>
2024-08-22 13:28:23 +0200

    feat: Add path to assign agents view in companies app

--- companies/urls.py
+++ companies/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
-from .views import CompanyListView, CompanyCreateView, CompanyDetailView, CompanyUpdateView, CompanyDeleteView
+from .views import (
+    CompanyListView, 
+    CompanyCreateView, 
+    CompanyDetailView, 
+    CompanyUpdateView, 
+    CompanyDeleteView, 
+    CompanyLeadsListView, 
+    CompanyTeamsListView,
+    company_assign_agents,
+
+)
+
 
 app_name = "companies"
+
 
 urlpatterns = [
     path("", CompanyListView.as_view(), name="company-list"),
@@ -9,4 +21,8 @@ urlpatterns = [
     path("<slug:company_slug>/", CompanyDetailView.as_view(), name="company-detail"),
     path("<slug:company_slug>/update/", CompanyUpdateView.as_view(), name="company-update"),
     path("<slug:company_slug>/delete/", CompanyDeleteView.as_view(), name="company-delete"),
+    path("<slug:company_slug>/leads/", CompanyLeadsListView.as_view(), name="company-leads"),
+    path("<slug:company_slug>/teams/", CompanyTeamsListView.as_view(), name="company-teams"),
+    path("<slug:company_slug>/assign/agents/", company_assign_agents, name="company-agents-assign"),
+
 ]
