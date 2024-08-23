9c39824cff8b7d019f4068eefacdbb195272a0ab
nxfive <kontakt@nxfive.pl>
2024-08-23 15:31:47 +0200

    feat: Create customize ManagerAdmin witch inherit from BaseUserAdmin

--- managers/admin.py
+++ managers/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .forms import ManagerForm
+from .models import Manager
+from core.admin import BaseUserAdmin
+
+
+class ManagerAdmin(BaseUserAdmin):
+    form = ManagerForm
+    list_display = BaseUserAdmin.list_display + (
+        "get_team",
+        "get_members_amount",
+    )
+
+    def get_team(self, obj: Manager) -> str | None:
+        if obj:
+            return obj.team
+
+    def get_members_amount(self, obj: Manager) -> int | None:
+        if obj:
+            return obj.team.members_count - 1
+
+    get_team.short_description = "Team"
+    get_members_amount.short_description = "Agents"
+
+    ordering = ("team", "user__last_name")
+
+
+admin.site.register(Manager, ManagerAdmin)
