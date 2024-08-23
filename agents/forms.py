11f5b9dd407a0f82d17ca5b946b72f41da877342
nxfive <kontakt@nxfive.pl>
2024-08-23 19:50:42 +0200

    feat: Create BaseAgentForm, add validation to role/team fields

--- agents/forms.py
+++ agents/forms.py
@@ -1,18 +1,42 @@
+from typing import Any
 from django import forms
+from django.core.exceptions import ValidationError
 
 from core.forms import BaseUserForm, Agent
+from teams.models import Team
 
 
-class AgentCreateForm(BaseUserForm):
+class BaseAgentForm(forms.ModelForm):
     class Meta:
         model = Agent
-        fields = BaseUserForm.Meta.fields + ("birth_date", "role", "team")
-        widgets = {
-            "birth_date": forms.DateInput(attrs={"type":"date"})
-        }
+        fields = ("role", "team")
+
+    def __init__(self, *args, **kwargs):
+        if self.__class__ == BaseUserForm:
+            raise NotImplementedError("BaseAgentForm cannot be instantiated directly.")
+        super().__init__(*args, **kwargs)
+
+        self.fields["team"].empty_label = "Select Team"
+
+    def clean(self) -> dict[str, Any]:
+        cleaned_data = super().clean()
+        role = cleaned_data.get("role")
+        team = cleaned_data.get("team")
+
+        if role == Agent.Roles.SALES and team != Team.TeamTypes.SALES:
+            raise ValidationError("Sales agent must be assigned to Sales team.")
+
+        if role == Agent.Roles.SUPPORT and team != Team.TeamTypes.SUPPORT:
+            raise ValidationError(
+                "Sales Support agent must be assigned to Sales Support team."
+            )
 
 
-class AgentUpdateForm(forms.ModelForm):
-    class Meta:
-        model = Agent
-        fields = ("role",)
+class AgentCreateForm(BaseAgentForm, BaseUserForm):
+    class Meta(BaseUserForm.Meta, BaseAgentForm.Meta):
+        fields = BaseUserForm.Meta.fields + BaseAgentForm.Meta.fields
+
+
+class AgentUpdateForm(BaseAgentForm):
+    class Meta(BaseAgentForm.Meta):
+        exclude = ("user",)
