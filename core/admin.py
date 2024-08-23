905748327fc8fd2f4a0728d31d7a26f8003f825a
nxfive <kontakt@nxfive.pl>
2024-08-23 15:18:12 +0200

    feat: Create admin class for user-related models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+
+class BaseUserAdmin(admin.ModelAdmin):
+    """
+        Base admin class for user-related models.
+
+        This class should only be used as a base class for other ModelAdmin classes
+        and should not be registered directly with the Django admin site.
+    """
+    list_display = ("get_last_name", "get_first_name", "get_email",)
+    list_display_links = ("get_last_name", "get_email",)
+
+    def get_last_name(self, obj):
+        return obj.user.last_name
+    
+    def get_first_name(self, obj):
+        return obj.user.first_name
+    
+    def get_email(self, obj):
+        return obj.user.email
+    
+    get_last_name.short_description = "Last Name"
+    get_first_name.short_description = "First Name"
+    get_email.short_description = "Email"
