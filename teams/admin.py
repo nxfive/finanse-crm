413be04ad6038be02f60881e036ff91371fdb6b2
nxfive <kontakt@nxfive.pl>
2024-08-23 18:09:58 +0200

    feat: Customize TeamAdmin with AgentInline

--- teams/admin.py
+++ teams/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .forms import TeamForm
+from .models import Team
+from agents.models import Agent
+
+
+class AgentInline(admin.TabularInline):
+    model = Agent
+    readonly_fields = ("role",)
+    extra = 0
+
+
+class TeamAdmin(admin.ModelAdmin):
+    form = TeamForm
+    inlines = (AgentInline, )
+    list_display = ("name", "team_type", "slug", "manager")
+
+
+admin.site.register(Team, TeamAdmin)
