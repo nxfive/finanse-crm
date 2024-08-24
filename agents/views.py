f5cf353001f55bf1195cba121d1a01fe489e2f15
nxfive <kontakt@nxfive.pl>
2024-08-24 17:49:14 +0200

    feat: Create views for assign/unassign companies to/from agent

--- agents/views.py
+++ agents/views.py
@@ -1,12 +1,29 @@
 from typing import Any
+from django.contrib import messages
+from django.core.exceptions import PermissionDenied
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView, UpdateView, ListView, DetailView, DeleteView
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import (
+    CreateView,
+    UpdateView,
+    ListView,
+    DetailView,
+    DeleteView,
+)
 from django.urls import reverse_lazy
 
 from .models import Agent
-from .forms import AgentCreateForm, AgentUpdateForm
+from .forms import (
+    AgentCreateForm,
+    AgentUpdateForm,
+    AgentCompaniesAssignForm,
+    AgentCompaniesUnassignForm,
+)
+from .utils import process_companies
 from core.mixins import AdminRequiredMixin
 from core.utils import paginate_queryset
+from teams.models import Team
 
 
 class AgentListView(LoginRequiredMixin, ListView):
@@ -17,7 +34,9 @@ class AgentListView(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        context["agents"] = paginate_queryset(request=self.request, queryset=self.get_queryset(), pages=self.paginate_by)
+        context["agents"] = paginate_queryset(
+            request=self.request, queryset=self.get_queryset(), pages=self.paginate_by
+        )
         return context
 
 
@@ -31,10 +50,10 @@ class AgentUpdateView(LoginRequiredMixin, AdminRequiredMixin, UpdateView):
     model = Agent
     form_class = AgentUpdateForm
     template_name = "agents/agent_update.html"
-    
+
     def get_success_url(self) -> str:
         return reverse_lazy("agents:agent-detail", kwargs={"pk": self.kwargs["pk"]})
-    
+
 
 class AgentDetailView(LoginRequiredMixin, DetailView):
     model = Agent
@@ -47,3 +66,33 @@ class AgentDeleteView(LoginRequiredMixin, AdminRequiredMixin, DeleteView):
     template_name = "agents/agent_delete.html"
     context_object_name = "agent"
     success_url = reverse_lazy("agents:agent-list")
+
+
+def agent_assign_companies(
+    request: HttpRequest, team_slug: str, pk: int
+) -> HttpResponseRedirect:
+    if request.user.is_manager:
+        return process_companies(
+            request,
+            team_slug,
+            pk,
+            form_class=AgentCompaniesAssignForm,
+            operation="assign",
+            template_name="agents/agent_assign_company.html",
+        )
+    raise PermissionDenied("You do not have permission")
+
+
+def agent_unassign_companies(
+    request: HttpRequest, team_slug: str, pk: int
+) -> HttpResponseRedirect:
+    if request.user.is_manager:
+        return process_companies(
+            request,
+            team_slug,
+            pk,
+            form_class=AgentCompaniesUnassignForm,
+            operation="unassign",
+            template_name="agents/agent_unassign_company.html",
+        )
+    raise PermissionDenied("You do not have permission")
