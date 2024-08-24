6dcc574b4479ac112d3088db90752dca90a34981
nxfive <kontakt@nxfive.pl>
2024-08-24 11:14:21 +0200

    feat: Add many to many rel to companies in Agent model

--- agents/models.py
+++ agents/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
 from accounts.models import Account
 from teams.models import Team
+from companies.models import Company
 
 
 class Agent(models.Model):
@@ -13,6 +15,7 @@ class Agent(models.Model):
     user = models.OneToOneField(Account, on_delete=models.CASCADE)
     role = models.CharField(max_length=30, choices=Roles.choices)
     team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True, related_name="agents")
+    companies = models.ManyToManyField(Company, blank=True, related_name="agents")
 
     class Meta:
         ordering = ("user__last_name", "user__first_name", )
