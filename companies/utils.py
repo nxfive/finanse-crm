b81a7a6331383071842501a4130205669aeb7f23
nxfive <kontakt@nxfive.pl>
2024-08-24 13:19:10 +0200

    feat: Create funcs to assign/unassign teams/agents
    Create separeate func to create messsage and to check if there
    are some items to assign/unassign for/from Company.

--- companies/utils.py
+++ companies/utils.py
@@ -1,12 +1,18 @@
 from django.contrib import messages
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import redirect, render
-
+from django.db.models.query import QuerySet
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
+from django.forms import Form
 from leads.forms import CompanyLeadCreateForm
 from leads.models import LeadSubmission
+from .models import Company
+from teams.models import Team
+from agents.models import Agent
 
 
-def company_lead_create(request: HttpRequest, template_name: str, success_url: str) -> HttpResponse:
+def company_lead_create(
+    request: HttpRequest, template_name: str, success_url: str
+) -> HttpResponse:
     if request.method == "POST":
         form = CompanyLeadCreateForm(request.POST)
         if form.is_valid():
@@ -19,8 +25,94 @@ def company_lead_create(request: HttpRequest, template_name: str, success_url: s
                 http_user_agent=request.META.get("HTTP_USER_AGENT"),
             )
 
-            messages.success(request, "Thank you for your message. We will contact you shortly.")
+            messages.success(
+                request, "Thank you for your message. We will contact you shortly."
+            )
             return redirect(success_url)
     else:
         form = CompanyLeadCreateForm()
     return render(request, template_name, {"form": form})
+
+
+def create_message(operation: str, field_name: str, items: QuerySet) -> str:
+    messages = {
+        "assign": "successfully assigned to the company.",
+        "unassign": "successfully unassigned from the company.",
+    }
+    return "{} {}".format(
+        field_name[:-1].capitalize() if items.count() == 1 else field_name.capitalize(),
+        messages[operation],
+    )
+
+
+def check_items_to_unassign(request: HttpRequest, field_name: str, company: Company, redirect_url: str) -> HttpResponseRedirect | None:
+    if (field_name == "agents" and not company.agents.exists()) or (field_name == "teams" and not company.teams.exists()):
+        messages.info(request, f"The company has no assigned {field_name}.")
+        return redirect(redirect_url, company_slug=company.slug)
+    
+
+def check_items_to_assign(request: HttpRequest, field_name: str, company: Company, redirect_url: str) -> HttpResponseRedirect | None:
+    agents = Agent.objects.filter(team__in=company.teams.all()).exclude(companies__in=[company])
+    teams = Team.objects.exclude(pk__in=company.teams.values_list("pk", flat=True))
+
+    if (field_name == "agents" and not agents) or (field_name == "teams" and not teams):
+        messages.info(request, f"There are no {field_name} available to assign to this company.")
+        return redirect(redirect_url, company_slug=company.slug)
+    
+
+def unassign_from_company(
+    request: HttpRequest,
+    company_slug: str,
+    form_class: Form,
+    field_name: str,
+    template_name: str,
+    redirect_url: str,
+) -> HttpResponse:
+    company = get_object_or_404(Company, slug=company_slug)
+
+    if request.method == "POST":
+        form = form_class(request.POST, company=company)
+        if form.is_valid():
+            items = form.cleaned_data.get(field_name)
+            for item in items:
+                item.companies.remove(company)
+
+            messages.success(request, create_message("unassign", field_name, items))
+            return redirect(redirect_url, company_slug=company_slug)
+    else:
+        response = check_items_to_unassign(request, field_name, company, redirect_url)
+        if response:
+            return response
+
+        form = form_class(company=company)
+
+    return render(request, template_name, context={"company": company, "form": form})
+
+
+def assign_to_company(
+    request: HttpRequest,
+    company_slug: str,
+    form_class: Form,
+    field_name: str,
+    template_name: str,
+    redirect_url: str,
+) -> HttpResponse:
+    company = get_object_or_404(Company, slug=company_slug)
+
+    if request.method == "POST":
+        form = form_class(request.POST, company=company)
+        if form.is_valid():
+            items = form.cleaned_data.get(field_name)
+            for item in items:
+                item.companies.add(company)
+
+            messages.success(request, create_message("assign", field_name, items))
+            return redirect(redirect_url, company_slug=company_slug)
+    else:
+        response = check_items_to_assign(request, field_name, company, redirect_url)
+        if response:
+            return response
+
+        form = form_class(company=company)
+
+    return render(request, template_name, context={"company": company, "form": form})
