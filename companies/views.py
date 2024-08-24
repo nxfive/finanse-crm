f0eb64be2cabb16ba9d5f85acb0a03f7680e3f42
nxfive <kontakt@nxfive.pl>
2024-08-24 13:10:31 +0200

    feat: Create views for assign/unassing agents and teams

--- companies/views.py
+++ companies/views.py
@@ -5,15 +5,28 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models.base import Model as Model
 from django.db.models.query import QuerySet
 from django.forms import BaseModelForm
+from django.core.exceptions import PermissionDenied
 from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DetailView,
+    DeleteView,
+)
 
-from .utils import company_lead_create
+from .utils import company_lead_create, unassign_from_company, assign_to_company
 from .models import Company
-from .forms import CompanyForm, CompanyAssignAgentsForm, CompanyUnassignAgentsForm, CompanyAssignTeamsForm, CompanyUnassignTeamsForm
+from .forms import (
+    CompanyForm,
+    CompanyAssignAgentsForm,
+    CompanyUnassignAgentsForm,
+    CompanyAssignTeamsForm,
+    CompanyUnassignTeamsForm,
+)
 from teams.models import Team
 from leads.models import Lead
 from core.utils import paginate_queryset
@@ -22,13 +35,24 @@ from core.decorators import check_user_team
 
 
 def bank_finanse_create(request: HttpRequest) -> HttpResponse:
-    return company_lead_create(request=request, template_name="websites/bank_finanse.html", success_url="websites:bank-finanse-sent")
+    return company_lead_create(
+        request=request,
+        template_name="websites/bank_finanse.html",
+        success_url="websites:bank-finanse-sent",
+    )
+
 
 def bank_finanse_sent(request: HttpRequest) -> HttpResponse:
     return render(request=request, template_name="websites/bank_finanse_sent.html")
 
+
 def house_finder_create(request: HttpRequest) -> HttpResponse:
-    return company_lead_create(request=request, template_name="websites/house_finder.html", success_url="websites:house-finder-sent")
+    return company_lead_create(
+        request=request,
+        template_name="websites/house_finder.html",
+        success_url="websites:house-finder-sent",
+    )
+
 
 def house_finder_sent(request: HttpRequest) -> HttpResponse:
     return render(request=request, template_name="websites/house_finder_sent.html")
@@ -47,7 +71,7 @@ class CompanyListView(LoginRequiredMixin, ListView):
         if self.kwargs.get("team_slug"):
             self.team = get_object_or_404(Team, slug=self.kwargs["team_slug"])
         return super().dispatch(request, *args, **kwargs)
-    
+
     def get_queryset(self) -> QuerySet[Any]:
         if self.team:
             return self.team.companies.all()
@@ -57,9 +81,11 @@ class CompanyListView(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         if self.team:
             context["team"] = self.team
-        context["companies"] = paginate_queryset(request=self.request, queryset=self.get_queryset(), pages=self.paginate_by)
+        context["companies"] = paginate_queryset(
+            request=self.request, queryset=self.get_queryset(), pages=self.paginate_by
+        )
         return context
-    
+
 
 class CompanyCreateView(LoginRequiredMixin, AdminRequiredMixin, CreateView):
     template_name = "companies/company_create.html"
@@ -69,7 +95,7 @@ class CompanyCreateView(LoginRequiredMixin, AdminRequiredMixin, CreateView):
     def form_valid(self, form: BaseModelForm) -> HttpResponse:
         messages.success(self.request, "Company successfully created")
         return super().form_valid(form)
-    
+
 
 class CompanyDetailView(LoginRequiredMixin, DetailView):
     template_name = "companies/company_detail.html"
@@ -87,7 +113,7 @@ class CompanyDetailView(LoginRequiredMixin, DetailView):
             messages.error(request, "Company does not exist.")
             return redirect("companies:company-list")
         return super().dispatch(request, *args, **kwargs)
-    
+
 
 class CompanyUpdateView(LoginRequiredMixin, AdminRequiredMixin, UpdateView):
     template_name = "companies/company_update.html"
@@ -96,7 +122,7 @@ class CompanyUpdateView(LoginRequiredMixin, AdminRequiredMixin, UpdateView):
 
     def get_object(self, queryset: QuerySet[Any] | None = ...) -> Model:
         return get_object_or_404(Company, slug=self.kwargs.get("company_slug"))
-    
+
     def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
         try:
             self.object = self.get_object()
@@ -104,10 +130,13 @@ class CompanyUpdateView(LoginRequiredMixin, AdminRequiredMixin, UpdateView):
             messages.error(request, "Company does not exist.")
             return redirect("companies:company-list")
         return super().dispatch(request, *args, **kwargs)
-    
+
     def get_success_url(self) -> str:
-        return reverse_lazy("companies:company-detail", kwargs={"company_slug": self.kwargs["company_slug"]})
-    
+        return reverse_lazy(
+            "companies:company-detail",
+            kwargs={"company_slug": self.kwargs["company_slug"]},
+        )
+
 
 class CompanyDeleteView(LoginRequiredMixin, AdminRequiredMixin, DeleteView):
     template_name = "companies/company_delete.html"
@@ -116,7 +145,7 @@ class CompanyDeleteView(LoginRequiredMixin, AdminRequiredMixin, DeleteView):
 
     def get_object(self, queryset: QuerySet[Any] | None = ...) -> Model:
         return get_object_or_404(Company, slug=self.kwargs.get("company_slug"))
-    
+
     def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
         try:
             self.object = self.get_object()
@@ -134,16 +163,18 @@ class CompanyTeamsListView(LoginRequiredMixin, AdminRequiredMixin, ListView):
     def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
         self.object = get_object_or_404(Company, slug=kwargs.get("company_slug"))
         return super().dispatch(request, *args, **kwargs)
-    
+
     def get_queryset(self) -> QuerySet[Any]:
-        return self.object.teams_assign.all()
-    
+        return self.object.teams.all()
+
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context["company"] = self.object
-        context["teams"] = paginate_queryset(request=self.request, queryset=self.get_queryset(), pages=self.paginate_by)
+        context["teams"] = paginate_queryset(
+            request=self.request, queryset=self.get_queryset(), pages=self.paginate_by
+        )
         return context
-    
+
 
 class CompanyLeadsListView(LoginRequiredMixin, AdminRequiredMixin, ListView):
     template_name = "companies/company_leads_list.html"
@@ -154,105 +185,67 @@ class CompanyLeadsListView(LoginRequiredMixin, AdminRequiredMixin, ListView):
 
     def get_queryset(self) -> QuerySet[Any]:
         return Lead.objects.filter(company__slug=self.kwargs["company_slug"]).all()
-    
+
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context["company"] = Company.objects.get(slug=self.kwargs["company_slug"])
-        context["leads"] = paginate_queryset(request=self.request, queryset=self.get_queryset(), pages=self.paginate_by)
+        context["leads"] = paginate_queryset(
+            request=self.request, queryset=self.get_queryset(), pages=self.paginate_by
+        )
         return context
 
 
 @login_required
 def company_assign_agents(request: HttpRequest, company_slug: str) -> HttpResponse:
-    company = get_object_or_404(Company, slug=company_slug)
-
-    if request.method == "POST":
-        form = CompanyAssignAgentsForm(request.POST, company=company)
-        if form.is_valid():
-            agents = form.cleaned_data.get("agents")
-            for agent in agents:
-                agent.companies.add(company)
-            if agents.count() == 1:
-                messages.success(request, "Agent successfully asigned to the company.")
-            else:
-                messages.success(request, "Agents successfully asigned to the company.")
-            return redirect("companies:company-detail", company_slug=company_slug)
-    else:
-        if len(company.get_agents) == 0:
-            messages.info(request, "There are no agents available to assign to this company.")
-            return redirect("companies:company-detail", company_slug=company_slug)
-        
-        form = CompanyAssignAgentsForm(company=company)
-    return render(request, "companies/company_assign_agent.html", context={"company": company, "form": form})
-
-
-@login_required
-def company_unassign_agents(request: HttpRequest, company_slug: str) -> HttpResponse:
-    company = get_object_or_404(Company, slug=company_slug)
-
-    if request.method == "POST":
-        form = CompanyUnassignAgentsForm(request.POST, company=company)
-        if form.is_valid():
-            agents = form.cleaned_data.get("agents")
-            for agent in agents:
-                agent.companies.remove(company)
-            if agents.count() == 1:
-                messages.success(request, "Agent successfully unasigned from the company.")
-            else:
-                messages.success(request, "Agents successfully unasigned from the company.")
-            return redirect("companies:company-detail", company_slug=company_slug)
-    else:
-        if len(company.get_agents) == 0:
-            messages.info(request, "This company has no assigned agents.")
-            return redirect("companies:company-detail", company_slug=company_slug)
-        
-        form = CompanyUnassignAgentsForm(company=company)
-    return render(request, "companies/company_unassign_agent.html", context={"company": company, "form": form})
+    if request.user.is_superuser:
+        return assign_to_company(
+            request,
+            company_slug,
+            form_class=CompanyAssignAgentsForm,
+            field_name="agents",
+            template_name="companies/company_assign_agent.html",
+            redirect_url="companies:company-detail",
+        )
+    raise PermissionDenied("You do not have permission to access this page.")
 
 
 @login_required
 def company_assign_teams(request: HttpRequest, company_slug: str) -> HttpResponse:
-    company = get_object_or_404(Company, slug=company_slug)
-
-    if request.method == "POST":
-        form = CompanyAssignTeamsForm(request.POST, company=company)
-        if form.is_valid():
-            teams = form.cleaned_data.get("teams")
-            for team in teams:
-                team.companies.add(company)
-            if teams.count() == 1:
-                messages.success(request, "Team successfully asigned to the company.")
-            else:
-                messages.success(request, "Teams successfully asigned to the company.")
-            return redirect("companies:company-detail", company_slug=company_slug)
-    else:
-        if len(company.get_teams) == 0:
-            messages.info(request, "There are no teams available to assign to this company.")
-            return redirect("companies:company-detail", company_slug=company_slug)
-        
-        form = CompanyAssignTeamsForm(company=company)
-    return render(request, "companies/company_assign_team.html", context={"company": company, "form": form})
+    if request.user.is_superuser:
+        return assign_to_company(
+            request,
+            company_slug,
+            form_class=CompanyAssignTeamsForm,
+            field_name="teams",
+            template_name="companies/company_assign_team.html",
+            redirect_url="companies:company-detail",
+        )
+    raise PermissionDenied("You do not have permission to access this page.")
 
 
 @login_required
 def company_unassign_teams(request: HttpRequest, company_slug: str) -> HttpResponse:
-    company = get_object_or_404(Company, slug=company_slug)
+    if request.user.is_superuser:
+        return unassign_from_company(
+            request,
+            company_slug,
+            form_class=CompanyUnassignTeamsForm,
+            field_name="teams",
+            template_name="companies/company_unassign_team.html",
+            redirect_url="companies:company-detail",
+        )
+    raise PermissionDenied("You do not have permission to access this page.")
 
-    if request.method == "POST":
-        form = CompanyUnassignTeamsForm(request.POST, company=company)
-        if form.is_valid():
-            teams = form.cleaned_data.get("teams")
-            for team in teams:
-                team.companies.remove(company)
-            if teams.count() == 1:
-                messages.success(request, "Team successfully unasigned from the company.")
-            else:
-                messages.success(request, "Teams successfully unasigned from the company.")
-            return redirect("companies:company-detail", company_slug=company_slug)
-    else:
-        if len(company.get_teams) == 0:
-            messages.info(request, "This company has no assigned teams.")
-            return redirect("companies:company-detail", company_slug=company_slug)
-        
-        form = CompanyUnassignTeamsForm(company=company)
-    return render(request, "companies/company_unassign_team.html", context={"company": company, "form": form})
+
+@login_required
+def company_unassign_agents(request: HttpRequest, company_slug: str) -> HttpResponse:
+    if request.user.is_superuser:
+        return unassign_from_company(
+            request,
+            company_slug,
+            form_class=CompanyUnassignAgentsForm,
+            field_name="agents",
+            template_name="companies/company_unassign_agent.html",
+            redirect_url="companies:company-detail",
+        )
+    raise PermissionDenied("You do not have permission to access this page.")
