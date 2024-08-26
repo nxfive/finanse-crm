abfeee81540ce8dbe30e09721a2fd7c2380172c2
nxfive <kontakt@nxfive.pl>
2024-08-26 22:41:12 +0200

    feat: Add to list_distplay - agent, team, company and pk

--- leads/admin.py
+++ leads/admin.py
@@ -1,32 +1,28 @@
 from typing import Any
 from django.contrib import admin
 from .models import Lead
-from .forms import LeadUpdateForm, LeadCreateForm
+from .forms import LeadAdminForm
 
 
 class LeadAdmin(admin.ModelAdmin):
-    form = LeadUpdateForm
-    add_form = LeadCreateForm
+    form = LeadAdminForm
+    
     list_display = (
+        "pk",
         "first_name",
         "phone_number",
         "product",
         "status",
+        "team",
+        "agent",
+        "company",
         "created_at",
-        "updated_at",
     )
     list_display_links = (
         "first_name",
         "phone_number",
     )
     search_fields = ("phone_number",)
-
-    def get_form(
-        self, request: Any, obj: Any | None = ..., change: bool = ..., **kwargs: Any
-    ) -> Any:
-        if obj is None:
-            return self.add_form
-        return self.form
 
 
 admin.site.register(Lead, LeadAdmin)
