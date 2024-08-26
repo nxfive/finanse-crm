762b509effd7f39233462f497df70f04ff3905e5
nxfive <kontakt@nxfive.pl>
2024-08-26 20:44:08 +0200

    feat: Add check in LeadListView if agent is True to access my-leads

--- leads/views.py
+++ leads/views.py
@@ -1,10 +1,12 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.forms.models import BaseModelForm
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 from django.views.generic import (
     CreateView,
     UpdateView,
@@ -18,20 +20,28 @@ from core.utils import paginate_queryset
 from core.decorators import check_user_team
 
 
-class LeadListView(LoginRequiredMixin, ListView):
+class LeadListView(ListView):
     model = Lead
     template_name = "leads/lead_list.html"
     context_object_name = "leads"
     paginate_by = 8
     agent = False
 
+    @method_decorator(login_required)
     @method_decorator(check_user_team)
     def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
         self.team = kwargs.pop("team", None)
+
+        if self.agent:
+            if not self.team and request.user.is_superuser:
+                return redirect(reverse('leads:lead-list'))
+            if (self.team and request.user.is_superuser) or (self.team and not request.user in self.team.members):
+                return redirect(reverse('teams:leads:lead-list', kwargs={"team_slug": self.team.slug}))
+
         return super().dispatch(request, *args, **kwargs)
 
     def get_queryset(self) -> QuerySet[Any]:
-        if self.request.user.is_superuser:
+        if self.request.user.is_superuser and not self.team and not self.agent:
             return Lead.objects.all()
         if self.kwargs.get("agent", self.agent):
             return Lead.objects.filter(agent__user=self.request.user, team=self.team)
@@ -43,7 +53,7 @@ class LeadListView(LoginRequiredMixin, ListView):
         context["leads"] = paginate_queryset(request=self.request, queryset=self.get_queryset(), pages=self.paginate_by)
         context["is_agent"] = self.agent
         return context
-
+    
 
 class LeadCreateView(LoginRequiredMixin, CreateView):
     template_name = "leads/lead_create.html"
