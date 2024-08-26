5fc22cdac2f08a0154fe9d986ee886ac5c444b3d
nxfive <kontakt@nxfive.pl>
2024-08-26 22:37:41 +0200

    feat: Create access mixin for detail/update/del lead view

---
+++ leads/mixins.py
@@ -0,0 +1,48 @@
+from typing import Any
+from django.contrib import messages
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views import View
+
+from core.decorators import check_user_team
+from leads.models import Lead
+from teams.models import Team
+
+
+class AccessControlMixin:
+
+    @method_decorator(check_user_team)
+    def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        self.team = kwargs.pop("team", None)
+        lead = get_object_or_404(Lead, pk=kwargs.get("pk"))
+
+        response = self.handle_access(request, lead, self.team, self.agent)
+        if response:
+            return response
+
+        return super().dispatch(request, *args, **kwargs)
+    
+    def handle_access(self, request: HttpRequest, lead: Lead, team: Team, agent: bool) -> HttpResponseRedirect | None:
+        if agent:
+            
+            if (team and request.user.is_superuser) or (team and not request.user in team.members):
+                messages.error(request, "You are not allowed to update this lead.")
+                return redirect(reverse("teams:leads:lead-list", kwargs={"team_slug": team.slug}))
+            
+            if request.user.is_superuser and not team:
+                messages.error(request, "You are not allowed to update this lead.")
+                return redirect(reverse("leads:lead-list"))
+
+        if not request.user.is_superuser:
+            if team and team != lead.team:
+                messages.error(request, "You are not allowed to update this lead.")
+                return redirect("teams:leads:lead-list", team_slug=team.slug)
+            
+            if team and not request.user.is_manager:
+                if team == lead.team and lead.agent and lead.agent.user != request.user:
+                    messages.error(request, "You are not allowed to update this lead.")
+                    return redirect("teams:leads:lead-list", team_slug=team.slug)
+        
+        return None
