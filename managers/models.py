30116194548e502073e65826a94e9558e4861b44
nxfive <kontakt@nxfive.pl>
2024-08-28 20:53:28 +0200

    feat: Add delete() method in Manager model to set is_manager to False

--- managers/models.py
+++ managers/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-from django.core.exceptions import ObjectDoesNotExist
 
 from accounts.models import Account
+
 
 class Manager(models.Model):
     user = models.OneToOneField(Account, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
-    
-    def get_team(self):
-        try:
-            return self.team
-        except ObjectDoesNotExist:
-            return None
+
+    def delete(self, *args, **kwargs):
+        self.user.is_manager = False
+        self.user.save()
+        
+        super().delete(*args, **kwargs)
