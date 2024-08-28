32651f3b82220e0ad6dfb2449bb4f52074a3b6ec
nxfive <kontakt@nxfive.pl>
2024-08-28 20:52:23 +0200

    feat: Add save() method in ManagerForm to set is_manager to True

--- managers/forms.py
+++ managers/forms.py
@@ -1,4 +1,4 @@
-from django  import forms
+from typing import Any
 
 from core.forms import BaseUserForm, Manager
 
@@ -7,3 +7,15 @@ class ManagerForm(BaseUserForm):
     class Meta:
         model = Manager
         fields = BaseUserForm.Meta.fields
+
+    def save(self, commit: bool = True) -> Any:
+        manager = super().save(commit=False)
+        
+        if not manager.pk:
+            manager.user.is_manager = True
+
+        if commit:
+            manager.user.save() 
+            manager.save() 
+
+        return manager
