5427df519c6d69e477822c042b1219165912014c
nxfive <kontakt@nxfive.pl>
2024-08-28 20:50:48 +0200

    fix: Del get_team and use own attribute in Manager admin model

--- managers/admin.py
+++ managers/admin.py
@@ -8,19 +8,14 @@ from core.admin import BaseUserAdmin
 class ManagerAdmin(BaseUserAdmin):
     form = ManagerForm
     list_display = BaseUserAdmin.list_display + (
-        "get_team",
+        "team",
         "get_agents_amount",
     )
-
-    def get_team(self, obj: Manager) -> str | None:
-        if obj:
-            return obj.team
 
     def get_agents_amount(self, obj: Manager) -> int | None:
         if obj:
             return len(obj.team.agents.all())
 
-    get_team.short_description = "Team"
     get_agents_amount.short_description = "Agents"
 
     ordering = ("team", "user__last_name")
