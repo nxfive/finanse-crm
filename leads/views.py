4b5a6c0991fbf7cdc160f6b39be3d0f542ef97c6
nxfive <kontakt@nxfive.pl>
2024-08-28 22:43:34 +0200

    feat: Create views for assign/unassign team, agent to/from lead

--- leads/views.py
+++ leads/views.py
@@ -1,11 +1,12 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.core.exceptions import PermissionDenied
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.forms.models import BaseModelForm
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import redirect
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.decorators import method_decorator
 from django.urls import reverse, reverse_lazy
 from django.views.generic import (
@@ -15,7 +16,7 @@ from django.views.generic import (
     ListView,
     DetailView,
 )
-from .forms import LeadAdminForm, LeadCreateManagerForm, LeadUpdateManagerForm, LeadUpdateAgentForm 
+from .forms import LeadAdminForm, LeadCreateManagerForm, LeadUpdateManagerForm, LeadUpdateAgentForm, LeadAssignAgentForm, LeadAssignTeamForm
 from .models import Lead
 from .mixins import AccessControlMixin
 from core.utils import paginate_queryset
@@ -120,3 +121,94 @@ class LeadDeleteView(LoginRequiredMixin, AccessControlMixin, DeleteView):
     context_object_name = "lead"
     success_url = reverse_lazy("leads:lead-list")
     agent = False
+
+
+@login_required
+def lead_assign_team(request: HttpRequest, pk: int) -> HttpResponse | HttpResponseRedirect:
+    if not request.user.is_superuser:
+        messages.error(request, "You are not allowed to assign the team.")
+        return redirect("teams:leads:lead-list")
+    
+    lead = get_object_or_404(Lead, pk=pk)
+
+    if request.method == "POST":
+        form = LeadAssignTeamForm(request.POST, lead=lead)
+        if form.is_valid():
+            team = form.cleaned_data.get("team")
+            lead.team = team
+            lead.save()
+        messages.success(request, "You successfully assign team to the lead.")
+        return redirect("leads:lead-detail", pk=pk)
+    else:
+        if not lead.company.teams.all():
+            messages.info(request, "The lead's company has no assigned teams.")
+            return redirect("leads:lead-detail", pk=pk)
+    
+        form = LeadAssignTeamForm(lead=lead)
+    return render(request, "leads/lead_assign_team.html", {"form": form, "lead": lead})
+
+
+@login_required
+def lead_assign_agent(request: HttpRequest, pk: int, team_slug: str=None) -> HttpResponse | HttpResponseRedirect:
+    lead = get_object_or_404(Lead, pk=pk)
+
+    if not lead.team and request.user.is_superuser:
+        messages.error(request, "This lead is not assigned to any team.")
+        return redirect("leads:lead-detail", pk=pk)
+
+    if not request.user.is_superuser and lead.team and lead.team.manager.user != request.user:
+        messages.error(request, "You are not allowed to assign the agent.")
+        return redirect("teams:leads:lead-list")
+
+    if request.method == "POST":
+        form = LeadAssignAgentForm(request.POST, lead=lead)
+        if form.is_valid():
+            agent = form.cleaned_data.get("agent")
+            lead.agent = agent
+            lead.save()
+        messages.success(request, "You successfully assign agent to the lead.")
+        if request.user.is_superuser:
+            return redirect("leads:lead-detail", pk=pk)
+        return redirect("teams:leads:lead-detail", team_slug=team_slug, pk=pk)
+    else:
+        agents_assigned_to_company = [agent for agent in lead.team.agents.all() if lead.company in agent.companies.all()]
+
+        if not agents_assigned_to_company:
+            messages.info(request, "None of the agents on the lead's team are assigned to the lead's company.")
+            if request.user.is_superuser:
+                return redirect("leads:lead-detail", pk=pk)
+            return redirect("teams:leads:lead-detail", team_slug=team_slug, pk=pk)
+        
+        form = LeadAssignAgentForm(lead=lead)
+    return render(request, "leads/lead_assign_agent.html", {"form": form, "lead": lead})
+
+
+@login_required
+def lead_unassign_team(request: HttpRequest, pk: int) -> HttpResponseRedirect:
+    if not request.user.is_superuser:
+        messages.error(request, "You are not allowed to unassign the team.")
+        return redirect("teams:leads:lead-list")
+    
+    lead = get_object_or_404(Lead, pk=pk)
+
+    lead.team = None
+    lead.agent = None
+    lead.save()
+
+    return redirect("leads:lead-detail", pk=pk)
+
+
+@login_required
+def lead_unassign_agent(request: HttpRequest, pk: int, team_slug: str=None) -> HttpResponseRedirect:
+    lead = get_object_or_404(Lead, pk=pk)
+    
+    if not request.user.is_superuser or not lead.team or (lead.team and lead.team.manager.user != request.user):
+        messages.error(request, "You are not allowed to assign the team.")
+        return redirect("teams:leads:lead-list")
+
+    lead.agent = None
+    lead.save()
+
+    if request.user.is_superuser:
+        return redirect("leads:lead-detail", pk=pk)
+    return redirect("teams:leads:lead-detail", team_slug=team_slug, pk=pk)
