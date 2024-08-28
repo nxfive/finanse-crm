47e32899536d7212a052d9ccc2458c016f710984
nxfive <kontakt@nxfive.pl>
2024-08-28 12:14:03 +0200

    fix: Add / at the end of update/delete team urls

--- teams/urls.py
+++ teams/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path("", TeamListView.as_view(), name="team-list"),
     path("create/", TeamCreateView.as_view(), name="team-create"),
     path("<slug:team_slug>/", TeamDetailView.as_view(), name="team-detail"),
-    path("<slug:team_slug>/update", TeamUpdateView.as_view(), name="team-update"),
+    path("<slug:team_slug>/update/", TeamUpdateView.as_view(), name="team-update"),
     path("<slug:team_slug>/update-companies/", update_team_companies, name="team-companies-update"),
-    path("<slug:team_slug>/delete", TeamDeleteView.as_view(), name="team-delete"),
+    path("<slug:team_slug>/delete/", TeamDeleteView.as_view(), name="team-delete"),
 
     path("<slug:team_slug>/agents/", include("agents.urls", namespace="agents")),
     path("<slug:team_slug>/leads/", include("leads.urls", namespace="leads")),
