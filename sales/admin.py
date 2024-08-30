23ad306cb536122779b3c36aa48bd1a5b1b745bd
nxfive <kontakt@nxfive.pl>
2024-08-31 01:22:12 +0200

    feat: Create admin models from Sale and Calculation models

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Sale, Calculation
+
+
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ("client", "bank_product", "sale_date", "amount", "status")
+
+
+class CalculationAdmin(admin.ModelAdmin):
+    list_display = ("client", "bank_product__bank", "bank_product", "amount", "duration_years", "rate")
+
+
+admin.site.register(Sale, SaleAdmin)
+admin.site.register(Calculation, CalculationAdmin)
