0c517d3dff4f2ed5e464473691775a7fcad6dc57
nxfive <kontakt@nxfive.pl>
2024-08-31 01:16:58 +0200

    feat: Create model Sale with Status choices

--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from clients.models import Client
+from banks.models import BankProduct
 
-# Create your models here.
+
+class Sale(models.Model):
+    class Status(models.TextChoices):
+        NEW = "New", _("New"),
+        PENDING = "Pending", ("Pending")
+        APPROVED = "Approved", _("Approved")
+        REJECTED = "Rejected", _("Rejected")
+        MANUAL_CHECK = "Manual Check", _("Manual Check")
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name="sales")
+    bank_product = models.ForeignKey(BankProduct, on_delete=models.CASCADE, related_name='sales')
+    sale_date = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    duration_years = models.PositiveIntegerField()  
+    status = models.CharField(max_length=50, choices=Status.choices, default=Status.NEW)
+
+    def __str__(self):
+        return f"Sale {self.id} - {self.client} - {self.bank_product}"
