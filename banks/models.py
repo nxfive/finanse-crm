8914666ddf48cf8b71fd52b194c2a62b5f442743
nxfive <kontakt@nxfive.pl>
2024-09-02 12:41:01 +0200

    feat: Create Bank and BankProduct model with ProductType choices

--- banks/models.py
+++ banks/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from core.models import Address
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Bank(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    headquarters = models.CharField(max_length=30)
+    customer_service = models.CharField(max_length=15)
+    established = models.DateField()
+    chairman = models.CharField(max_length=60)
+    address = models.OneToOneField(Address, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class BankProduct(models.Model):
+    class ProductType(models.TextChoices):
+        MORTGAGE_LOAN = "Mortgage Loan", _("Mortgage Loan"),
+        PERSONAL_LOAN = "Personal Loan", _("Personal Loan"),
+        CREDIT_CARD = "Credit Card", _("Credit Card"),
+        INVESTMENTS = "Investments", _("Investments")
+
+    bank = models.ForeignKey(Bank, on_delete=models.CASCADE)
+    product_type = models.CharField(max_length=20, choices=ProductType.choices)
+    description = models.TextField(blank=True, null=True)
+    interest_rate = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    terms = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.get_product_type_display()
