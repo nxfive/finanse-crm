e6a5d7419cdb76e3565ac03c45fdf2210ae69f14
nxfive <kontakt@nxfive.pl>
2024-09-02 16:26:52 +0200

    feat: Change generic views to funcs, add url to process and sale

--- clients/urls.py
+++ clients/urls.py
@@ -1,17 +1,24 @@
 from django.urls import path
-from .views import ClientListView, ClientDetailView, ClientUpdateView, ClientDeleteView
+from .views import (
+    list_clients,
+    get_client,
+    create_client,
+    update_client,
+    delete_client,
+    process_client,
+)
 
+from sales.views import create_sale
 
 app_name = "clients"
 
 
 urlpatterns = [
-    path("", ClientListView.as_view(), name="client-list"),
-    path("<int:pk>/", ClientDetailView.as_view(), name="client-detail"),
-    path("<int:pk>/update/", ClientUpdateView.as_view(), name="client-update"),
-    path("<int:pk>/delete/", ClientDeleteView.as_view(), name="client-delete"),
-
-    path("my-clients/", ClientListView.as_view(only_agent=True), name="my-clients"),
-    path("my-clients/<int:pk>/", ClientDetailView.as_view(only_agent=True), name="my-client-detail"),
-    path("my-clients/<int:pk>/update", ClientUpdateView.as_view(only_agent=True), name="my-client-update"),
+    path("", list_clients, name="client-list"),
+    path("create/", create_client, name="client-create"),
+    path("<int:pk>/", get_client, name="client-detail"),
+    path("<int:pk>/update/", update_client, name="client-update"),
+    path("<int:pk>/delete/", delete_client, name="client-delete"),
+    path("<int:pk>/process/", process_client, name="client-process"),
+    path("<int:pk>/add-sale/", create_sale, name="client-add-sale")
 ]
