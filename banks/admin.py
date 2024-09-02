541be66b52c75bcd99a9a6f942fea32fa2e855fa
nxfive <kontakt@nxfive.pl>
2024-09-02 12:46:40 +0200

    feat: Register Bank, BankProduct model in admin

--- banks/admin.py
+++ banks/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Bank, BankProduct
+
+
+class BankAdmin(admin.ModelAdmin):
+    list_display = ("name", "headquarters", "customer_service", "chairman",)
+
+class BankProductAdmin(admin.ModelAdmin):
+    list_display = ("bank", "product_type", "interest_rate",)
+
+
+
+admin.site.register(Bank, BankAdmin)
+admin.site.register(BankProduct, BankProductAdmin)
