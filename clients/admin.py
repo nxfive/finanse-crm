2f4d4acfbe55ebfdf922066695df82d22bd063bc
nxfive <kontakt@nxfive.pl>
2024-09-02 16:31:34 +0200

    feat: Register Client model in admin with config

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("user", "first_name", "last_name", "email", "phone_number", "birth_date", "salary",)
+    
+
+admin.site.register(Client, ClientAdmin)
