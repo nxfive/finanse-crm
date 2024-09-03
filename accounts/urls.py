3396ba7738b49b9599e1bd855cefba471c5d50a0
nxfive <kontakt@nxfive.pl>
2024-09-03 19:52:09 +0200

    feat: Create urls for account activation process

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,10 @@ from . import views
 urlpatterns = [
     path("", views.list_accounts, name="account-list"),
     path("dashboard/<slug:username>/", views.dashboard, name="dashboard"),
-    path("login/", views.custom_login, name="login"),
+    path("login/", views.login, name="login"),
+    path("signup/", views.signup, name="signup"),
+    path("activate/<uidb64>/<token>/", views.activate, name="activate"),
+    path("activation-sent/", views.activation_sent, name="activation-sent"),
+    path("activation-complete/", views.activation_complete, name="activation-complete"),
+    path("activation-invalid/", views.activation_invalid , name="activation-invalid"),
 ]
