5fcd9642157807a1d6b61bbb7e4932a35c616eec
nxfive <kontakt@nxfive.pl>
2024-09-03 21:44:04 +0200

    feat: Create UserProfile model

--- accounts/models.py
+++ accounts/models.py
@@ -71,3 +71,7 @@ class Account(AbstractBaseUser):
     def has_module_perms(self, app_label):
         return True
 
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(Account, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=f"photos/userprofile/{user.pk}/", default="photos/userprofile/default.png")
