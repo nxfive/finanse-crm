275e29580ca2d2c4bb8a2d232d14897cebc66f75
nxfive <kontakt@nxfive.pl>
2024-09-03 22:11:57 +0200

    feat: Add urls to password reset proces and logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,26 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth.views import (
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 
 urlpatterns = [
     path("", views.list_accounts, name="account-list"),
     path("dashboard/<slug:username>/", views.dashboard, name="dashboard"),
     path("login/", views.login, name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
     path("signup/", views.signup, name="signup"),
     path("activate/<uidb64>/<token>/", views.activate, name="activate"),
     path("activation-sent/", views.activation_sent, name="activation-sent"),
     path("activation-complete/", views.activation_complete, name="activation-complete"),
     path("activation-invalid/", views.activation_invalid , name="activation-invalid"),
+    path("password-reset/", PasswordResetView.as_view(), name="password_reset"),
+    path("password-reset/confirm/<uidb64>/<token>/", PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("password-reset/complete/", PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path("password-reset/done/", PasswordResetDoneView.as_view(), name="password_reset_done"),
 ]
