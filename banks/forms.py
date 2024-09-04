709c38268e0e8c3bf459686177605638f38a4aaa
nxfive <kontakt@nxfive.pl>
2024-09-04 11:25:16 +0200

    feat: Add BaseBankProductForm and create from it create/update forms

--- banks/forms.py
+++ banks/forms.py
@@ -15,8 +15,28 @@ class BankForm(forms.ModelForm):
         fields = ("name", "headquarters", "customer_service", "established", "chairman",)
 
 
-class BankProductForm(forms.ModelForm):
+class BaseBankProductForm(forms.ModelForm):
     class Meta:
         model = BankProduct
         fields = ("bank", "product_type", "description", "interest_rate", "terms",)
+        widgets = {
+            "interest_rate": forms.NumberInput(attrs={"placeholder": "0.00"}),
+        }
 
+    def __init__(self, *args, **kwargs):
+        if self.__class__ == BaseBankProductForm:
+            raise NotImplementedError("BaseBankProductForm cannot be instantiated directly.")
+        super().__init__(*args, **kwargs)
+
+
+class BankProductCreateForm(BaseBankProductForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["bank"].empty_label = "Select Bank"
+
+
+class BankProductUpdateForm(BaseBankProductForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["bank"].widget.attrs.update({"readonly": True, "disabled": True})
+        self.fields["product_type"].widget.attrs.update({"readonly": True, "disabled": True})
